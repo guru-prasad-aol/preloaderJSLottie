62af4cbc72f0999f0ad8b7c2d7d2905930097053
guru-prasad-aol <71254966+guru-prasad-aol@users.noreply.github.com>
2025-04-23 16:02:09 +0530

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -49,7 +49,7 @@ function initializePreloader() {
             renderer: 'canvas', // or 'canvas' if preferred
             loop: true,
             autoplay: true,
-            path: 'https://lottie.host/13227c8b-9d9c-477d-ac30-ab0f62190f38/ABkzG7NhCG.lottie',
+            path: 'https://lottie.host/b9831b55-ebe1-45b9-a675-8c3712359e5e/yg5M08le8c.lottie',
         });
     }
 
